428164aee28142c2bab01e9f5449b82a2794d70d
Roger Martínez <roger.runch@gmail.com>
2017-09-23 11:58:21 +0200

    Avoid bucle in canceled signed in

--- app/src/main/java/es/rogermartinez/friendlychat/MainActivity.java
+++ app/src/main/java/es/rogermartinez/friendlychat/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package es.rogermartinez.friendlychat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -235,5 +236,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Signed canceled!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 }
